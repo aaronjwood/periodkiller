6bcf82880b7429c6afbfceb3088a8593f17576d2
Aaron Wood <aaronjwood@gmail.com>
2013-03-28 23:32:39 -0400

    Finished implementing About window

--- Windows/About.xaml.cs
+++ Windows/About.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -9,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PeriodKiller.Windows
@@ -31,6 +33,12 @@ namespace PeriodKiller.Windows
         private void Close_Button_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void Hyperlink_AJW(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
