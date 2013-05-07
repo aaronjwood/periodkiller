777dacb50a0a70017385d323ffd7a8c83b5f4814
Aaron Wood <aaronjwood@gmail.com>
2013-05-06 23:38:13 -0400

    Finished building out all the windows and porting over all the code from the old WinForms version. Used animations for hiding showing controls at key points instead of manipulating the visibility. Updated the assembly info, version bump to 3.0, and targeted .NET 4 client profile. Building for AnyCPU. Fixed some performance issues by analyzing code analysis.

---
+++ Windows/Collisions.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PeriodKiller.Windows
+{
+    /// <summary>
+    /// Interaction logic for Collisions.xaml
+    /// </summary>
+    public partial class Collisions : Window
+    {
+        public Collisions()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Add a duplicate item to the list
+        /// </summary>
+        /// <param name="item">The array of items that should be added to the list</param>
+        public void addItem(string[] item)
+        {
+            this.duplicateItems.Items.Add(new { Type = item[0], Original = item[1], Renamed = item[2], Collides = item[3] });
+        }
+    }
+}
--- Windows/PeriodKiller.xaml.cs
+++ Windows/PeriodKiller.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using PeriodKiller.Windows;
+using PeriodKiller.Cleaners;
+using PeriodKiller.Class;
 
 namespace PeriodKiller.Windows
 {
@@ -24,6 +26,11 @@ namespace PeriodKiller.Windows
     {
         private System.Windows.Forms.FolderBrowserDialog folderDialog = new System.Windows.Forms.FolderBrowserDialog();
         private static string selectedFolder;
+        private FolderCleaner folderCleaner = new FolderCleaner();
+        private FilenameCleaner filenameCleaner = new FilenameCleaner();
+        private bool filenameProcessingEnabled = false;
+        private bool removeFolderTextEnabled = false;
+        private bool removeFilenameTextEnabled = false;
 
         public PeriodKiller()
         {
@@ -38,45 +45,109 @@ namespace PeriodKiller.Windows
         private void selectFolderButton_Click(object sender, RoutedEventArgs e)
         {
             //When a folder is selected, set the label, animate the window's width and height, and show additional controls
-            if (folderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            if (this.folderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 //Animate the window once a folder has been selected
                 if (selectedFolder == null)
                 {
-                    animateWindowHeight(Application.Current.MainWindow, 400, .5, new Action(() =>
+                    this.animateWindowHeight(Application.Current.MainWindow, 400, .5, new Action(() =>
                     {
-                        animateWindowWidth(Application.Current.MainWindow, 500, .5, new Action(() =>
+                        this.animateWindowWidth(Application.Current.MainWindow, 500, .5, new Action(() =>
                         {
-                            animateControlOpacity(selectedFolderLabel, 1, 1);
-                            animateControlOpacity(fixFoldersButton, 1, 1);
+                            this.animateControlOpacity(this.selectedFolderLabel, 1, 1);
+                            this.animateControlOpacity(this.fixFoldersButton, 1, 1);
                         }));
                     }));
                 }
 
                 //Set the path selected
                 selectedFolder = this.folderDialog.SelectedPath;
-                selectedFolderLabel.Content = selectedFolder;
+                this.selectedFolderLabel.Content = selectedFolder;
 
             }
         }
 
         /// <summary>
-        /// Event handler for button to being processing items
+        /// Event handler for button to begin processing items
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void fixFoldersButton_Click(object sender, RoutedEventArgs e)
         {
-            
-            //Make sure the user has selected a folder
-            if (selectedFolder != "")
+
+            //Hide this each time a fresh run is performed
+            this.duplicatesLabel.Cursor = Cursors.Arrow;
+            this.animateControlOpacity(this.duplicatesLabel, 0, .3);
+
+            //Make sure the directory hasn't been deleted outside of the program
+            if(!Directory.Exists(selectedFolder))
             {
+                this.selectedFolderLabel.Content = "Folder no longer exists!";
+                return;
+            }
 
-                //Make sure the directory hasn't been deleted outside of the program
-                if(!Directory.Exists(selectedFolder))
+            //Reset counts from previous run (if any)
+            folderCleaner.resetCounts();
+            filenameCleaner.resetCounts();
+
+            //Remove a string from each directory if option is checked and the text box isn't empty
+            if (this.removeFolderTextEnabled && this.removeFolderTextBox.Text != "")
+            {
+                folderCleaner.removeText(this.folderDialog, this.removeFolderTextBox.Text);
+            }
+
+            //Remove periods from directory names
+            folderCleaner.removePeriods(this.folderDialog);
+
+            //Remove a string from each file if option is checked and the text box isn't empty
+            if (this.removeFilenameTextEnabled && this.removeFilenameTextBox.Text != "")
+            {
+                filenameCleaner.removeText(this.folderDialog, this.removeFilenameTextBox.Text);
+            }
+
+            //Remove periods from filenames if the option has been selected
+            if (this.filenameProcessingEnabled)
+            {
+                filenameCleaner.removePeriods(this.folderDialog);
+            }
+
+            //If there are duplicates 
+            if (folderCleaner.Duplicates.Count > 0 || filenameCleaner.Duplicates.Count > 0)
+            {
+                duplicatesLabel.Content = (folderCleaner.Duplicates.Count + filenameCleaner.Duplicates.Count) + " collision(s) found when restructuring folders. Click here to view them.";
+                this.duplicatesLabel.Cursor = Cursors.Hand;
+                this.animateControlOpacity(this.duplicatesLabel, 1, .5);
+            }
+
+            //Display message about processed folders/files
+            Messages cleanerMessages = new Messages(folderCleaner.numPeriods, folderCleaner.numRenames, filenameCleaner.numPeriods, filenameCleaner.numRenames);
+            if (cleanerMessages.hasMessage())
+            {
+                MessageBox.Show(cleanerMessages.getMessage());
+            }
+        }
+
+        /// <summary>
+        /// Event handler to add duplicates to the collisions window and show it
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DuplicatesLabel_MouseLeftButtonUp(object sender, RoutedEventArgs e)
+        {
+            if(folderCleaner.Duplicates.Count > 0 || filenameCleaner.Duplicates.Count > 0)
+            {
+                Collisions collisions = new Collisions();
+                collisions.Owner = this;
+                foreach (string[] folder in folderCleaner.Duplicates)
                 {
-
+                    collisions.addItem(folder);
                 }
+                foreach (string[] file in filenameCleaner.Duplicates)
+                {
+                    collisions.addItem(file);
+                }
+                collisions.Width = this.Width;
+                collisions.ShowDialog();
             }
         }
 
@@ -151,6 +222,74 @@ namespace PeriodKiller.Windows
                 control.BeginAnimation(Label.OpacityProperty, controlAnimation);
             }), null);
             control.EndInit();
+        }
+
+        /// <summary>
+        /// Event handler for the checkable option to enable file processing in the menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Menu_ProcessFilenames_Checked(object sender, RoutedEventArgs e)
+        {
+            this.filenameProcessingEnabled = true;
+        }
+
+        /// <summary>
+        /// Event handler for the checkable option to disable file processing in the menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Menu_ProcessFilenames_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.filenameProcessingEnabled = false;
+        }
+
+        /// <summary>
+        /// Event handler for the checkable option to enable removing folder text in the menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Menu_RemoveFolderText_Checked(object sender, RoutedEventArgs e)
+        {
+            this.removeFolderTextEnabled = true;
+            this.animateControlOpacity(this.removeFolderTextLabel, 1, 1);
+            this.animateControlOpacity(this.removeFolderTextBox, 1, 1);
+        }
+
+        /// <summary>
+        /// Event handler for the checkable option to disable removing folder text in the menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Menu_RemoveFolderText_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.removeFolderTextEnabled = false;
+            this.animateControlOpacity(this.removeFolderTextLabel, 0, 1);
+            this.animateControlOpacity(this.removeFolderTextBox, 0, 1);
+        }
+
+        /// <summary>
+        /// Event handler for the checkable option to enable removing filename text in the menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Menu_RemoveFilenameText_Checked(object sender, RoutedEventArgs e)
+        {
+            this.removeFilenameTextEnabled = true;
+            this.animateControlOpacity(this.removeFilenameTextLabel, 1, 1);
+            this.animateControlOpacity(this.removeFilenameTextBox, 1, 1);
+        }
+
+        /// <summary>
+        /// Event handler for the checkable option to disable removing filename text in the menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Menu_RemoveFilenameText_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.removeFilenameTextEnabled = false;
+            this.animateControlOpacity(this.removeFilenameTextLabel, 0, 1);
+            this.animateControlOpacity(this.removeFilenameTextBox, 0, 1);
         }
 
         /// <summary>
